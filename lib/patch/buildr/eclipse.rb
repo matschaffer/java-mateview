febc756368d677d8a54b009815035399d97c2d0a
Mat Schaffer <mat.schaffer@gmail.com>
2009-09-21 19:38:06 -0400

    Added a comment to describe the eclipse buildr monkey patch.

--- lib/patch/buildr/eclipse.rb
+++ lib/patch/buildr/eclipse.rb
@@ -1,3 +1,6 @@
+# Monkey patch to get around buildr eclipse issue it be fixed in buildr 1.3.5
+# https://issues.apache.org/jira/browse/BUILDR-315
+
 require 'pathname'
 
 class Buildr::Eclipse::ClasspathEntryWriter
